3c697fbd62391d9befe21a1168538f1b1eeca842
elibixby <elibixby@google.com>
2016-05-02 17:36:21 -0700

    Fixed template and example config

--- gce/globally_available_service.py
+++ gce/globally_available_service.py
@@ -22,7 +22,7 @@ def gen_instance_template(name, instance_template):
     return {
         'name': name + '-instance-template',
         'type': 'compute.v1.instanceTemplate',
-        'properties': instance_template
+        'properties': {'properties': instance_template}
     }
 
 
@@ -73,16 +73,17 @@ def gen_firewall_rule(name, port):
     }
 
 
-def gen_target_pool(name, health_checks):
+def gen_target_pool(name, region, health_checks):
     return {
-        'name': name + '-target-pool',
+        'name': name + '-target-pool-' + region,
         'type': 'compute.v1.targetPool',
         'properties': {
+            'region': region,
             'healthChecks': [
                 SELF_LINK.format(health_check['name'])
                 for health_check in health_checks
             ],
-            'instances': {},
+            'instances': [],
             'description': (
                 'A target pool to provide health checks'
                 'to individual instances serving APIs'
@@ -95,16 +96,18 @@ def gen_target_pool(name, health_checks):
 
 
 def gen_global_forwarding_rule(name, target_proxy, port, ip_address=None):
-    return {
+    forwarding_rule = {
         'name': name + '-fowarding-rule-' + target_proxy['name'],
-        'type': 'compute.v1.globalFowardingRule',
+        'type': 'compute.v1.globalForwardingRule',
         'properties': {
             'target': SELF_LINK.format(target_proxy['name']),
             'portRange': port,
-            'IPAddress': ip_address,
             'IPProtocol': 'TCP',
         }
     }
+    if ip_address:
+        forwarding_rule['properties']['IPAddress'] = ip_address
+    return forwarding_rule
 
 
 def gen_url_map(name, backend_service, dns_name=''):
@@ -125,6 +128,7 @@ def gen_url_map(name, backend_service, dns_name=''):
             'pathMatchers': [{
                 'description': 'all paths',
                 'name': 'all',
+                'defaultService': service_link,
                 'pathRules': [{
                     'paths': ['/*'],
                     'service': service_link
@@ -140,19 +144,25 @@ def gen_backend_service(name,
                         health_check=None,
                         https=False):
     backends = [
-        dict(group=SELF_LINK.format(igm['name']), **backend_template)
-        for igm in igms
+        dict(
+            group=PROP.format(igm['name'], 'instanceGroup'),
+            **backend_template
+        ) for igm in igms
     ]
-    return {
+    backend_service = {
         'name': name + '-backend-service',
         'type': 'compute.v1.backendService',
         'properties': {
             'backends': backends,
-            'healthChecks': [health_check] if health_check else [],
             'portName': 'api-port',
             'protocol': 'HTTPS' if https else 'HTTP',
         }
     }
+    if health_check:
+        backend_service['properties']['healthChecks'] = [
+            SELF_LINK.format(health_check['name']),
+        ]
+    return backend_service
 
 
 def gen_instance_group_manager(name,
@@ -160,12 +170,13 @@ def gen_instance_group_manager(name,
                                instance_template,
                                target_pool=None):
     return {
-        'name': name + '-igm-' + instance_template['properties']['zone'],
+        'name': name + '-igm-' + zone,
         'type': 'compute.v1.instanceGroupManager',
         'properties': {
             'baseInstanceName': instance_template['name'],
             'instanceTemplate': SELF_LINK.format(instance_template['name']),
             'targetSize': 1,
+            'zone': zone,
             'targetPools': [
                 SELF_LINK.format(target_pool['name'])
             ] if target_pool else []
@@ -178,6 +189,7 @@ def gen_autoscaler(name, autoscaler_template, instance_group_manager):
         'name': name + instance_group_manager['name'] + '-autoscaler',
         'type': 'compute.v1.autoscaler',
         'properties': dict(
+            zone=instance_group_manager['properties']['zone'],
             target=SELF_LINK.format(instance_group_manager['name']),
             **autoscaler_template
         )
@@ -199,25 +211,38 @@ def GenerateConfig(context):
 
     health_check_template = context.properties.get('health_check')
 
-    # Only need a target pool if the user wants a health check
+    # Only need target pools if the user wants a health check
+
     health_check = None
     target_pool = None
     if health_check_template:
         health_check = gen_health_check(
             name, health_check_template, port, ssl=cert)
-        target_pool = gen_target_pool(name, [health_check])
         resources.append(health_check)
-        resources.append(target_pool)
+
+        regions = {
+            zone: zone.rsplit('-', 1)[0]
+            for zone in context.properties['zones']
+        }
+        target_pools = dict()
+        for region in set(regions.values()):
+            target_pool = gen_target_pool(name, region, [health_check])
+            target_pools[region] = target_pool
+            resources.append(target_pool)
 
     # Create a managed instance group in each zone
     igms = []
     for zone in context.properties['zones']:
         igm = gen_instance_group_manager(
-            name, zone, instance_template, target_pool=target_pool)
+            name,
+            zone,
+            instance_template,
+            target_pool=target_pools[regions[zone]]
+        )
         igms.append(igm)
         resources.append(igm)
 
-    # Optionally autoscale those instances
+    # Optionally autoscale those managed instance groups
     autoscaler_template = context.properties.get('autoscaler')
     if autoscaler_template:
         for igm in igms:
@@ -236,7 +261,7 @@ def GenerateConfig(context):
 
     # A trivial URL Map that only maps to the single backend service
     url_map = gen_url_map(
-        name, backend_service, dns_name=context.properties.get('dns_name'))
+        name, backend_service, dns_name=context.properties.get('dns_name', ''))
     resources.append(url_map)
 
     # A target proxy that connects the fowarding rule and the urlmap
