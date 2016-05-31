c1163168ce0683471fa8220b23b4e3199eda5ae8
Mingliang Wei <mlwei@google.com>
2016-05-31 16:07:27 -0700

    Original Firebase quickstart example for Android. https://github.com/firebase/quickstart-android/tree/fad847bdd476911598932b49516e1f3e97deb6ec/auth
    Change-Id: If414fa8b75ff602d1a2e7b7e92c60ab2871e5f91

---
+++ examples/client/firebase-android/README.md
@@ -0,0 +1,115 @@
+Firebase Auth Quickstart
+==============================
+
+Introduction
+------------
+
+- [Read more about Firebase Auth](https://firebase.google.com)
+
+Getting Started
+---------------
+
+- [Add Firebase to your Android Project](https://firebase.google.com/docs/android/setup).
+
+
+### Google Sign In Setup
+
+- Go to the [Firebase Console](https://console.firebase.google.com) and navigate to your project:
+  - Select the **Auth** panel and then click the **Sign In Method** tab.
+  - Click **Google** and turn on the **Enable** switch, then click **Save**.
+- Run the app on your device or emulator.
+    - Select `GoogleSignInActivity` from the main screen.
+    - Click the **Sign In** button to begin.
+
+
+### Facebook Login Setup
+
+- Go to the [Facebook Developers Site](https://developers.facebook.com) and follow all
+  instructions to set up a new Android app. When asked for a package name, use
+  `com.google.firebase.quickstart.usermanagement` and when asked for a main class name,
+  use `com.google.firebase.quickstart.usermanagement.FacebookLoginActivity`.
+- Go to the [Firebase Console](https://console.firebase.google.com) and navigate to your project:
+  - Select the **Auth** panel and then click the **Sign In Method** tab.
+  - Click **Facebook** and turn on the **Enable** switch, then click **Save**.
+  - Enter your Facebook **App Id** and **App Secret** and click **Save**.
+- Open the file `app/src/main/res/values/ids.xml` and replace the value of the `facebook_app_id` with the ID of the Facebook app you just created.
+- Run the app on your device or emulator.
+    - Select `FacebookLoginActivity` from the main screen.
+    - Click the **Sign In** button to begin.
+    - If you see text that says Facebook is disabled, make sure you are running
+      either the `facebookDebug` or `facebookRelease` variants of this sample.
+
+
+### Twitter Login Setup
+
+- Go to the [Twitter Developers Site](https://dev.twitter.com) and follow all
+  instructions to set up a new Android app. Make sure to enable Twitter Login when appropriate.
+- Go to the [Firebase Console](https://console.firebase.google.com) and navigate to your project:
+  - Select the **Auth** panel and then click the **Sign In Method** tab.
+  - Click **Twitter** and turn on the **Enable** switch, then click **Save**.
+  - Enter your Twitter **API Key** and **API Secret** and click **Save**.
+- Open the file `app/src/main/res/values/ids.xml` and replace the value of the `twitter_consumer_key`
+  and `twitter_consumer_secret` with the key and secret you just put into the Firebase console.
+- Run the app on your device or emulator.
+    - Select `TwitterLoginActivity` from the main screen.
+    - Click the **Log In with Twitter** button to begin.
+
+### Email/Password Setup
+
+- Go to the [Firebase Console](https://console.firebase.google.com) and navigate to your project:
+  - Select the **Auth** panel and then click the **Sign In Method** tab.
+  - Click **Email/Password** and turn on the **Enable** switch, then click **Save**.
+- Run the app on your device or emulator.
+    - Select `EmailPasswordActivity` from the main screen.
+    - Fill in your desired email and password and click **Create Account** to begin.
+
+
+### Custom Authentication Setup
+
+- Go to the [Google Developers Console](https://console.developers.google.com/project) and navigate to your project:
+    - From the left "hamburger" menu navigate to the **API Manager** tab.
+    - Click on the **Credentials** item in the left column.
+    - Click **New credentials** and select **Service account key**. Select **New service account**,
+    pick any name, and select **JSON** as the key type. Then click **Create**.
+    - You should now have a new JSON file for your service account in your Downloads directory.
+- Open the file `web/auth.html` in your computer's web browser.
+    - Click **Choose File** and upload the JSON file you just downloaded.
+    - Enter any User ID and click **Generate**.
+    - Copy the text from the **ADB Command** section, you will need this.
+- Run the Android application on your Android device or emulator.
+    - Select `CustomAuthActivity` from the main screen.
+    - Run the text you copied from the **ADB Command** section of the web page in the steps above.
+      This should send a token to the running app and update the Custom Token field.
+    - Click **Sign In** to sign in to Firebase User Management with the generated JWT. You should
+      see the User ID you entered when generating the token.
+
+
+Screenshots
+-----------
+<img src="app/src/screen.png" height="534" width="300"/>
+
+Support
+-------
+
+https://firebase.google.com/support/
+
+License
+-------
+
+Copyright 2016 Google, Inc.
+
+Licensed to the Apache Software Foundation (ASF) under one or more contributor
+license agreements.  See the NOTICE file distributed with this work for
+additional information regarding copyright ownership.  The ASF licenses this
+file to you under the Apache License, Version 2.0 (the "License"); you may not
+use this file except in compliance with the License.  You may obtain a copy of
+the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+License for the specific language governing permissions and limitations under
+the License.
+
---
+++ examples/client/firebase-android/app/src/main/java/com/google/firebase/quickstart/auth/AnonymousAuthActivity.java
@@ -0,0 +1,238 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.firebase.quickstart.auth;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * Activity to demonstrate anonymous login and account linking (with an email/password account).
+ */
+public class AnonymousAuthActivity extends BaseActivity implements
+        View.OnClickListener {
+
+    private static final String TAG = "AnonymousAuth";
+
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+
+    // [START declare_auth_listener]
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    // [END declare_auth_listener]
+
+    private EditText mEmailField;
+    private EditText mPasswordField;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anonymous_auth);
+
+        // [START initialize_auth]
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+
+        // [START auth_state_listener]
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // [START_EXCLUDE]
+                updateUI(user);
+                // [END_EXCLUDE]
+            }
+        };
+        // [END auth_state_listener]
+
+        // Fields
+        mEmailField = (EditText) findViewById(R.id.field_email);
+        mPasswordField = (EditText) findViewById(R.id.field_password);
+
+        // Click listeners
+        findViewById(R.id.button_anonymous_sign_in).setOnClickListener(this);
+        findViewById(R.id.button_anonymous_sign_out).setOnClickListener(this);
+        findViewById(R.id.button_link_account).setOnClickListener(this);
+    }
+
+    // [START on_start_add_listener]
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    // [END on_start_add_listener]
+
+    // [START on_stop_remove_listener]
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    // [END on_stop_remove_listener]
+
+    private void signInAnonymously() {
+        showProgressDialog();
+        // [START signin_anonymously]
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInAnonymously:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInAnonymously", task.getException());
+                            Toast.makeText(AnonymousAuthActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // [START_EXCLUDE]
+                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END signin_anonymously]
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+        updateUI(null);
+    }
+
+    private void linkAccount() {
+        // Make sure form is valid
+        if (!validateLinkForm()) {
+            return;
+        }
+
+        // Get email and password from form
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+
+        // Create EmailAuthCredential with email and password
+        AuthCredential credential = EmailAuthProvider.getCredential(email, password);
+
+        // Link the anonymous user to the email credential
+        showProgressDialog();
+        // [START link_credential]
+        mAuth.getCurrentUser().linkWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "linkWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(AnonymousAuthActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // [START_EXCLUDE]
+                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END link_credential]
+    }
+
+    private boolean validateLinkForm() {
+        boolean valid = true;
+
+        String email = mEmailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmailField.setError("Required.");
+            valid = false;
+        } else {
+            mEmailField.setError(null);
+        }
+
+        String password = mPasswordField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPasswordField.setError("Required.");
+            valid = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+
+        return valid;
+    }
+
+    private void updateUI(FirebaseUser user) {
+        hideProgressDialog();
+
+        TextView idView = (TextView) findViewById(R.id.anonymous_status_id);
+        TextView emailView = (TextView) findViewById(R.id.anonymous_status_email);
+        boolean isSignedIn = (user != null);
+
+        // Status text
+        if (isSignedIn) {
+            idView.setText(getString(R.string.id_fmt, user.getUid()));
+            emailView.setText(getString(R.string.email_fmt, user.getEmail()));
+        } else {
+            idView.setText(R.string.signed_out);
+            emailView.setText(null);
+        }
+
+        // Button visibility
+        findViewById(R.id.button_anonymous_sign_in).setEnabled(!isSignedIn);
+        findViewById(R.id.button_anonymous_sign_out).setEnabled(isSignedIn);
+        findViewById(R.id.button_link_account).setEnabled(isSignedIn);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_anonymous_sign_in:
+                signInAnonymously();
+                break;
+            case R.id.button_anonymous_sign_out:
+                signOut();
+                break;
+            case R.id.button_link_account:
+                linkAccount();
+                break;
+        }
+    }
+}
